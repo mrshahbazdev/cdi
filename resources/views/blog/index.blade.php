c2e145819cfe1b13cbf773bcbc603b0d418a79e7
unknown <mr.shahbaznns@gmail.com>
2025-12-27 11:22:38 +0500

    post editor upload

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,4 +1,3 @@
-@section('title', 'Blog & Insights – SaaS, Automatisierung | Digitalpackt')
 @section('meta_description', 'Fachartikel, Tutorials und Expertenwissen zu SaaS-Plattformen, Automatisierung, digitalen Produkten und skalierbaren Architekturen bei Digitalpackt.')
 <x-app-layout
     title="Blog & Insights – SaaS, Automatisierung | Digitalpackt"
