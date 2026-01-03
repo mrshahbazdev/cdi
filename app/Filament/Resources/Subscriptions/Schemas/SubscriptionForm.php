cd3ccc97769a4a68ec4af94ece2f557e95881b3c
unknown <mr.shahbaznns@gmail.com>
2026-01-03 11:50:59 +0500

    section edit solve admin subscription

--- app/Filament/Resources/Subscriptions/Schemas/SubscriptionForm.php
+++ app/Filament/Resources/Subscriptions/Schemas/SubscriptionForm.php
@@ -3,12 +3,12 @@
 namespace App\Filament\Resources\Subscriptions\Schemas;
 
 use Filament\Schemas\Schema;
-use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\DateTimePicker;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Select;
+use Filament\Schemas\Components\TextInput;
+use Filament\Schemas\Components\Textarea;
+use Filament\Schemas\Components\Toggle;
 
 class SubscriptionForm
 {
